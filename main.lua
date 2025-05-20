494fb98012499dad77cd693362f9019171194c3d
bocaj111004 <bocaj111004@gmail.com>
2025-05-20 17:54:16 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -193,7 +193,7 @@ function Library:AddESP(Parameters)
 			end
 		end
 		local function Setline(Line, Width, ColorToSet, Origin, Destination, Border)
-			if Highlight then
+			if Highlight and pos ~= nil then
 				local Position = (Origin + Destination) / 2
 				Line.Position = UDim2.new(0, Position.X, 0, Position.Y)
 				local Length = (Origin - Destination).Magnitude
@@ -213,7 +213,7 @@ function Library:AddESP(Parameters)
 		local ConnectionCooldown = false
 		local Connection = RunService.Heartbeat:Connect(function()
 
-			local pos = (Object:IsA("Model") and Object.PrimaryPart and Object.PrimaryPart.Position or Object:IsA("Model") and Object.WorldPivot.Position or Object.Position)
+			local pos = (Object:IsA("Model") and Object.PrimaryPart ~= nil and Object.PrimaryPart.Position or Object:IsA("Model") and Object.WorldPivot.Position or Object.Position)
 
 
 			local ScreenPoint, OnScreen = game.Workspace.CurrentCamera:WorldToScreenPoint(pos)
